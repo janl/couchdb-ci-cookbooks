9b445aac6db1f36213966dd7807e8520e82ed3cb
till <till@php.net>
2013-06-15 21:05:20 +0200

    Bugfix: let chef handle the provider part

--- cookbooks/couchdb-ci/recipes/default.rb
+++ cookbooks/couchdb-ci/recipes/default.rb
@@ -18,15 +18,12 @@ when "centos","fedora"
               "esl-erlang", #"erlang-etap", "erlang-erts", "erlang-os_mon", "erlang-eunit",
               "libicu-devel", "js-devel", "curl-devel",
               "git"]
-  package_provider = Chef::Provider::Package::Yum
+
 else
   Chef::Log.error("#{node["platform"]} is currently not supported")
   raise
 end
 
 packages.each do |package_name|
-  package "Installing #{package_name} via #{package_provider}" do
-    name package_name
-    provider package_provider
-  end
+  package package_name
 end
